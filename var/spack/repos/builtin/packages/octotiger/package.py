8a9552d95d5c9208a86413c803d574543299d2e7
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2020-03-26 16:49:45 +0100

    version master and passing the OCTOTIGER_WITH_CUDA flag

--- var/spack/repos/builtin/packages/octotiger/package.py
+++ var/spack/repos/builtin/packages/octotiger/package.py
@@ -5,9 +5,10 @@ class Octotiger(CMakePackage):
     homepage = "https://github.com/STEllAR-GROUP/octotiger"
     url = "https://github.com/STEllAR-GROUP/octotiger"
 
-    # version('master', git='https://github.com/STEllAR-GROUP/hpx.git', branch='master')
     version('octotiger_with_kokkos', git='https://github.com/STEllAR-GROUP/octotiger.git',
         branch='octotiger_with_kokkos')
+    version('master', git='https://github.com/STEllAR-GROUP/octotiger.git',
+        branch='master')
 
     variant('cuda', default=True, 
             description='Build octotiger fmm kernels with CUDA directly, not only the Kokkos/HPX parallel magic')
@@ -18,20 +19,26 @@ class Octotiger(CMakePackage):
     depends_on('boost')
     depends_on('hdf5@:1.10.999 +mpi +cxx')
     depends_on('silo+mpi')#^hdf5+cxx+mpi')
-    depends_on('kokkos @3.0 +serial +cuda +cuda_lambda +hpx +wrapper')#, when='@octotiger_with_kokkos') #cxxstd=c++14 # call e.g. for daint  with ^kokkos+pascal60+hsw 
-    depends_on('kokkos-nvcc-wrapper', patches='Add-dumpversion-option-to-nvcc_wrapper.patch')
+    depends_on('kokkos @3.0 +serial +cuda +cuda_lambda +hpx +wrapper', when='@octotiger_with_kokkos') #cxxstd=c++14 # call e.g. for daint  with ^kokkos+pascal60+hsw 
+    depends_on('kokkos-nvcc-wrapper', patches='Add-dumpversion-option-to-nvcc_wrapper.patch', when='@octotiger_with_kokkos')
     depends_on('cmake@3.10:', type='build')
     depends_on('cuda', when='+cuda')
 
     def cmake_args(self):
         spec = self.spec
         args = []
+	
+        # CUDA
+        args.append('-DOCTOTIGER_WITH_CUDA={0}'.format(
+            'ON' if ('+cuda' in spec) else 'OFF'
+        )) #TODO
 
         # Kokkos
         args.append('-DOCTOTIGER_WITH_KOKKOS={0}'.format(
             'ON' if '@octotiger_with_kokkos' else 'OFF'
         ))
         # set nvcc_wrapper as compiler
-        args.append("-DCMAKE_CXX_COMPILER=%s" % self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
+        if '@octotiger_with_kokkos':
+            args.append("-DCMAKE_CXX_COMPILER=%s" % self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
 
         return args
